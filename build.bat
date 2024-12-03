885329c009af91a593dbba07adc47ddba9a64774
MdTanjeemHaider <mdtanjeem.haider@ontariotechu.net>
2024-12-02 22:38:24 -0500

    Fix build file

--- build.bat
+++ build.bat
@@ -5,7 +5,6 @@ pyinstaller ^
     --workpath build ^
     --specpath build ^
     --distpath build ^
-    --add-data "../hand_landmarker.task;." ^
     --add-data "../audio/*;audio" ^
     --hidden-import "msvc-runtime" ^
     --collect-data "mediapipe" ^
